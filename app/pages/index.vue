22d8740581276cced31f1e02a08ce4dbf26bbd7e
pete-jordan <106772966+pete-jordan@users.noreply.github.com>
2022-12-01 10:41:53 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
-
+        <!--
         <div class="mb-12 xl:mb-0">
           <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
 
@@ -32,6 +32,7 @@
             </button>
           </form>
         </div>
+        -->
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
         <img
